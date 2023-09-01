4f55f5a951cafc74b8a6e4eb6b5bc7ff6c57a3f8
EhtishamFaisal-SF <89829993+EhtishamFaisal-SF@users.noreply.github.com>
2023-09-01 17:20:14 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ sed -i 's/20015/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: minhng.info | Live chat port: '$CHAT
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: 9*E"+f9XBrB@UU | Live chat port: '$CHAT
